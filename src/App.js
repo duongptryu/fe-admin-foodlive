e9067b155c856673927d19e071421d50df8ad322
duongptryu <duongptryu@gmail.com>
2022-03-24 17:21:07 +0700

    duongpt - add screen payment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "antd/dist/antd.css";
 import { Routes, Route } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
 import Template from "./layout/layout";
 import UserManage from "./component/UserManager/index";
 import OwnerRstManager from "./component/OwnerRstManager";
 import RstManager from "./component/RstManager";
 import CategoryManager from "./component/CategoryManager";
 import DetailRst from "./component/RstDetail";
+import * as PusherPushNotifications from "@pusher/push-notifications-web";
+import PaymentScreen from "./component/Payment/payment";
+
+const beamsClient = new PusherPushNotifications.Client({
+  instanceId: "d1a00704-2838-48a8-984d-a3cd63dafce7",
+});
+
+const beamsTokenProvider = new PusherPushNotifications.TokenProvider({
+  url: "http://localhost:8080/api/v1/beams",
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjM2LCJyb2xlIjoiU1RBRkYifSwiZXhwIjoxNjUwMTg1NjA0LCJpYXQiOjE2NDc1OTM2MDR9.BPafNMzKH6HpdDE6SiqUxIKmGH0hb8rG_oq9abL8oBc", // Headers your auth endpoint needs
+  },
+});
 
 function App() {
   return (
@@ -38,6 +52,12 @@ function App() {
             exact={true}
             path="/category"
             element={<CategoryManager></CategoryManager>}
+          />
+
+          <Route
+            exact={true}
+            path="/order"
+            element={<PaymentScreen></PaymentScreen>}
           />
         </Routes>
       </div>
---
+++ src/component/Payment/payment.js
@@ -0,0 +1,13 @@
+import { Row, Col } from "antd";
+
+const PaymentScreen = () => {
+  return (
+    <Row style={{ paddingTop: "10%" }}>
+      <Col span={12} offset={6}>
+        col-12 col-offset-6
+      </Col>
+    </Row>
+  );
+};
+
+export default PaymentScreen;
